f6618b8fb351cd3aeb52e6d29e5e149f68d3670e
Luis Marsano <luis.marsano@gmail.com>
2018-08-08 01:34:40 -0400

    simplify index.test
    import db-test-helper

--- index.test.js
+++ index.test.js
@@ -1,35 +1,14 @@
 'use strict'
-import test from 'ava'
+import {setup, test} from './db-test-helper'
 import url from 'url'
 import request from 'supertest'
-import MongodbMemoryServer from 'mongodb-memory-server'
-import mongoose from 'mongoose'
-
 import init from '.'
 
-const mongod = new MongodbMemoryServer
-    , instance = (() => {
-	let i = 0
-	return () => i++
-})()
-    , tap = (value) => {
-	console.error('trace', value)
-	return value
-}
-let port
-
-test.before(async () => {
-	port = await mongod.getPort()
-})
+setup()
 
 test.beforeEach(async (t) => {
 	const {context} = t
-	    , connection = context.connection
-	                 = await mongoose.createConnection
-	( tap(`mongodb://localhost:${port}/test${instance()}`)
-	, {useNewUrlParser: true}
-	)
-	    , app = await init(connection)
+	    , app = await init(context.connection)
 	    , client = request.agent(app.listen())
 	    , uri = '/api/user'
 	    , userCreate = { uri
@@ -40,28 +19,6 @@ test.beforeEach(async (t) => {
 	                                    })
 	                   }
 	Object.assign(context, {client, userCreate})
-})
-
-test.afterEach.always(async (t) => {
-	const {connection} = t.context
-	try {
-		await connection.dropDatabase()
-		connection.close()
-	} catch (e) {
-		e.message += `
-Failed to drop database & closed connection.`
-		throw e
-	}
-})
-
-test.after.always(async () => {
-	try {
-		await mongod.stop()
-	} catch (e) {
-		e.message += `
-Failed to stop mongo.`
-		throw e
-	}
 })
 
 test
